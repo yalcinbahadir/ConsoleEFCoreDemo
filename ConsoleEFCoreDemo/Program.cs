d6ad7840b9e5bc1505c8a5339af272a993a46711
yalcinPXL <yalcinPXL@gmail.com>
2021-06-30 23:11:40 +0200

    Mock repo implementation.

--- ConsoleEFCoreDemo/Program.cs
+++ ConsoleEFCoreDemo/Program.cs
@@ -1,5 +1,6 @@
 ﻿using ConsoleEFCoreDemo.Data;
 using ConsoleEFCoreDemo.Entities;
+using ConsoleEFCoreDemo.Repositories;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Linq;
@@ -10,146 +11,28 @@ namespace ConsoleEFCoreDemo
     {
         static void Main(string[] args)
         {
-            // ShowDepartmentsAndEmployees();
-            //AddEmployee(new Employee() { Name="Test", LastName="Tester", DepartmentId=2 });
-            //var emp = GetEmployee(4);
-            //if (emp !=null)
+            //var employees=EmployeeMockRepository.GetEmployees();
+
+            //foreach (var employee in employees)
+            //{
+            //    Console.WriteLine($" Employee :{employee.Name} {employee.LastName} Department : {employee.Department.Name}");
+            //}
+
+            //EmployeeMockRepository.AddEmployee(new Employee() { Name = "Test", LastName = "Tester", DepartmentId = 2 });
+            //var emp = EmployeeMockRepository.GetEmployee(6);
+            //if (emp != null)
             //{
             //    Console.WriteLine(emp.Name);
             //}
 
-            //DeletEmployee(emp);
-            //////Update
-            //var newEmp=AddEmployee(new Employee() { Name = "Test", LastName = "Tester", DepartmentId = 2 });
+            //EmployeeMockRepository.DeletEmployee(emp);
+
+            ////Update
+            //var newEmp = EmployeeMockRepository.AddEmployee(new Employee() { Name = "Demo", LastName = "Edit-demo", DepartmentId = 2 });
             //newEmp.Name = "Editor";
-            //var updated=UpdateEmployee(newEmp);
+            //var updated = EmployeeMockRepository.UpdateEmployee(newEmp);
             //Console.WriteLine(updated.Name);
             Console.ReadLine();
         }
-
-        private static Employee GetEmployee(int id)
-        {
-            using (var context = new AppDbContext())
-            {
-                var employee = context.Employees.Include(e => e.Department).FirstOrDefault(e=>e.Id==id);
-                if (employee ==null)
-                {
-                    Console.WriteLine("Not found.");
-                    return null;
-                }
-
-                return employee;
-            }
-        }
-
-        private static void DeletEmployee(Employee employee)
-        {
-            if (employee == null)
-            {
-                Console.WriteLine("Bad request. Employee can not be null.");
-                return;
-            }
-            using (AppDbContext context = new AppDbContext())
-            {
-                var existing = GetEmployee(employee.Id);
-                if (existing == null)
-                {
-                    Console.WriteLine("Employee not found.");
-                    return;
-                }
-
-                context.Employees.Remove(employee);
-                var isremoved = context.SaveChanges() > 0;
-                if (!isremoved)
-                {
-                    Console.WriteLine($"Employee is not deleted.");                   
-                    return;
-                }
-
-                Console.WriteLine($"Employee {employee.Name} {employee.LastName} was successfully deleted.");
-
-            }
-        }
-
-        private static Employee AddEmployee(Employee employee)
-        {
-            if (employee == null)
-            {
-                Console.WriteLine("Bad request. Employee can not be null.");
-                return employee;
-            }
-            using (AppDbContext context=new AppDbContext())
-            {
-
-                var result=context.Employees.Add(employee);
-                var isAdded = context.SaveChanges() > 0;
-                if (isAdded)
-                {
-                    Console.WriteLine($"Employee {employee.Name} {employee.LastName} was successfully recorded.");
-                }
-                else
-                {
-                    Console.WriteLine($"Employee is not recorded.");
-                }
-                return result.Entity;
-            }
-        }
-
-        private static void ShowDepartmentsAndEmployees()
-        {
-            using (var context = new AppDbContext())
-            {
-                var departments = context.Departments.Include(d => d.Employees).ToList();
-
-                foreach (var department in departments)
-                {
-                    Console.WriteLine($"Departments");
-                    Console.WriteLine($"ID- {department.DepartmentId} Name - {department.Name}");
-                    Console.WriteLine($"Employees");
-                    if (department.Employees.Any())
-                    {
-                        foreach (var employee in department.Employees)
-                        {
-                            Console.WriteLine($"ID- {employee.Id} Name - {employee.Name} {employee.LastName}");
-                        }
-
-                    }
-                }
-            }
-        }
-
-        private static Employee UpdateEmployee(Employee employee)
-        {
-            if (employee == null)
-            {
-                Console.WriteLine("Bad request. Employee can not be null.");
-                return employee;
-            }
-            using (AppDbContext context = new AppDbContext())
-            {
-                var existing = GetEmployee(employee.Id);
-                if (existing == null)
-                {
-                    Console.WriteLine("Employee not found.");
-                    return employee;
-                }
-
-                existing.Name = employee.Name;
-                existing.LastName = employee.LastName;
-                existing.DepartmentId = employee.DepartmentId;
-                context.Employees.Update(existing);
-                var isUpdated = context.SaveChanges() > 0;
-                if (!isUpdated)
-                {
-                    Console.WriteLine($"Employee is not updated.");
-                    return employee;
-                }
-
-                Console.WriteLine($"Employee {existing.Name} {existing.LastName} was successfully updated.");
-                return existing;
-            }
-        }
-
-
     }
 }
---
+++ ConsoleEFCoreDemo/Repositories/EmployeeMockRepository.cs
@@ -0,0 +1,123 @@
+﻿using ConsoleEFCoreDemo.Data;
+using ConsoleEFCoreDemo.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleEFCoreDemo.Repositories
+{
+    public static class EmployeeMockRepository
+    {
+        public static Employee GetEmployee(int id)
+        {
+            using (var context = new AppDbContext())
+            {
+                var employee = context.Employees.Include(e => e.Department).FirstOrDefault(e => e.Id == id);
+                if (employee == null)
+                {
+                    Console.WriteLine("Not found.");
+                    return null;
+                }
+
+                return employee;
+            }
+        }
+
+        public static void DeletEmployee(Employee employee)
+        {
+            if (employee == null)
+            {
+                Console.WriteLine("Bad request. Employee can not be null.");
+                return;
+            }
+            using (AppDbContext context = new AppDbContext())
+            {
+                var existing = GetEmployee(employee.Id);
+                if (existing == null)
+                {
+                    Console.WriteLine("Employee not found.");
+                    return;
+                }
+
+                context.Employees.Remove(employee);
+                var isremoved = context.SaveChanges() > 0;
+                if (!isremoved)
+                {
+                    Console.WriteLine($"Employee is not deleted.");
+                    return;
+                }
+
+                Console.WriteLine($"Employee {employee.Name} {employee.LastName} was successfully deleted.");
+
+            }
+        }
+
+        public static Employee AddEmployee(Employee employee)
+        {
+            if (employee == null)
+            {
+                Console.WriteLine("Bad request. Employee can not be null.");
+                return employee;
+            }
+            using (AppDbContext context = new AppDbContext())
+            {
+
+                var result = context.Employees.Add(employee);
+                var isAdded = context.SaveChanges() > 0;
+                if (isAdded)
+                {
+                    Console.WriteLine($"Employee {employee.Name} {employee.LastName} was successfully recorded.");
+                }
+                else
+                {
+                    Console.WriteLine($"Employee is not recorded.");
+                }
+                return result.Entity;
+            }
+        }
+
+        public static List<Employee> GetEmployees()
+        {
+            using (var context = new AppDbContext())
+            {
+                var departments = context.Employees.Include(d => d.Department).ToList();
+                return departments;
+            }
+        }
+
+        public static Employee UpdateEmployee(Employee employee)
+        {
+            if (employee == null)
+            {
+                Console.WriteLine("Bad request. Employee can not be null.");
+                return employee;
+            }
+            using (AppDbContext context = new AppDbContext())
+            {
+                var existing = GetEmployee(employee.Id);
+                if (existing == null)
+                {
+                    Console.WriteLine("Employee not found.");
+                    return employee;
+                }
+
+                existing.Name = employee.Name;
+                existing.LastName = employee.LastName;
+                existing.DepartmentId = employee.DepartmentId;
+                context.Employees.Update(existing);
+                var isUpdated = context.SaveChanges() > 0;
+                if (!isUpdated)
+                {
+                    Console.WriteLine($"Employee is not updated.");
+                    return employee;
+                }
+
+                Console.WriteLine($"Employee {existing.Name} {existing.LastName} was successfully updated.");
+                return existing;
+            }
+        }
+    }
+}
